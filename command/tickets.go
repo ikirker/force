5d79c07d791999e356c07a5e5be08e9741c3fc6f
Ian Kirker <ian.kirker@gmail.com>
2019-10-07 00:51:48 +0100

    Adds tickets open to go straight to a ticket console URL

--- command/tickets.go
+++ command/tickets.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	desktop "github.com/ForceCLI/force/desktop"
 	. "github.com/ForceCLI/force/error"
 	. "github.com/ForceCLI/force/lib"
 )
@@ -25,6 +26,8 @@ Usage:
 
   force tickets cli
 
+  force tickets open <ticket id or num>
+
   force tickets _completion 
 
   force tickets _completion <partial>
@@ -45,7 +48,7 @@ func runTickets(cmd *Command, args []string) {
 		case "cli":
 			ErrorAndExit("not yet implemented")
 		case "_completion":
-			runCompletion(args[1:])
+			runTicketsCompletion(args[1:])
 			if len(args) == 3 {
 				createBulkInsertJob(args[2], args[1], "CSV", "Parallel")
 			} else if len(args) == 4 {
@@ -91,9 +94,47 @@ func runTicketsWaiting(args []string) {
 	//force, _ := ActiveForce()
 }
 
-func runCompletion(args []string) {
+func runTicketsCompletion(args []string) {
 	if len(args) < 2 {
 		ErrorAndExit("must specify object and id")
 	}
 	//force, _ := ActiveForce()
+}
+
+func runTicketsOpen(args []string) {
+	if len(args) > 3 {
+		ErrorAndExit("only one id or number please")
+	}
+	ticketArg := args[2]
+
+	// In theory this isn't foolproof but it seems to work
+	if ticketArg[0] == "a" {
+		argType := "Id"
+	} else {
+		argType := "Name"
+	}
+
+	// The URL field you want is BMCServiceDesk__Launch_console__c
+	//  but it comes wrapped as an <a href=""></a>
+	force, _ := ActiveForce()
+	soql := fmt.Sprintf("SELECT %s,BMCServiceDesk__Launch_console__c FROM BMCServiceDesk__Incident__c WHERE %s = '%s' LIMIT 1", argType, argType, ticketArg[0])
+	records, err := force.Query(fmt.Sprintf("%s", soql))
+
+	if err != nil {
+		ErrorAndExit(err.Error())
+	} else {
+		// Temporary: for showing guts
+		fmt.Printf("%+v\n", records)
+	}
+	ticketAnchor := records["BMCServiceDesk__Launch_console__c"]
+	ticketAnchorParts := strings.Split(ticketAnchor, "\"")
+	if len(ticketAnchorParts) != 3 {
+		ErrorAndExit("ticket URL was not in the format we expect\n")
+	}
+	ticketURL := ticketAnchorParts[1]
+
+	err := desktop.Open(ticketURL)
+	if err != nil {
+		ErrorAndExit(err.Error())
+	}
 }
